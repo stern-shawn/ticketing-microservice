c7d144bc850df8fb50d39c67101d06b35dd30c0a
Shawn Stern <shawn.stern0@walmartlabs.com>
2020-06-30 20:27:13 -0700

    add startup message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,8 @@ import { app } from './app';
 const PORT = 3000;
 
 const start = async () => {
+  console.log('Starting up auth service...');
+
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY not defined!');
   }
