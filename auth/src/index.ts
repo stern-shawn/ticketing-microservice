9333d53d40830eea0e3f2fd9e7750c425a957172
Shawn Stern <shawn.stern0@walmartlabs.com>
2020-06-30 22:12:12 -0700

    more dots

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,7 +5,7 @@ import { app } from './app';
 const PORT = 3000;
 
 const start = async () => {
-  console.log('Starting up auth service...');
+  console.log('Starting up auth service......');
 
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY not defined!');
